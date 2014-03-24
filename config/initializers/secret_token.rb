493a04d3e34d23ea936b23e64b09528b599cf66b
7rrf <sm7rrfmail.com>
2014-03-24 16:03:14 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0031880165a0a42a15a9a416ab45cad1d4f65d614efe2ff598a080df30f49e19c46089e2bbc484f7462a31d944e2c7c978d5cc8b3e0c76f1f99497899b625eb4'
